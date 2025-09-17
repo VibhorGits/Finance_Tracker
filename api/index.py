3c7258d3692f7f7a586afc51bc870a4640e9f576
VibhorGits <vibhor.bhatt1705@gmail.com>
2025-09-17 18:32:17 +0530

    Finalee

--- api/index.py
+++ api/index.py
@@ -178,7 +178,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "finance-tracker-gyrp-git-e-6f4220-bhattvibhor123-9492s-projects.vercel.app",
+    "https://finance-tracker-gyrp-git-e-6f4220-bhattvibhor123-9492s-projects.vercel.app",
 ]
 
 # Add the middleware to your app
